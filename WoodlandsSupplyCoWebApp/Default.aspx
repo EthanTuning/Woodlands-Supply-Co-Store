7de65462b6a60aee2706ed53228248cf31eb6028
Ethan Tuning <etuning5@hotmail.com>
2018-05-24 22:28:35 -0700

    got menu working, got animations working. finalized design

--- WoodlandsSupplyCoWebApp/Default.aspx
+++ WoodlandsSupplyCoWebApp/Default.aspx
@@ -2,4 +2,29 @@
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
+        <section class="hero is-fullheight">
+            <div id="hero-image" class="hero-body">
+                <div class="container">
+                    <h1 id="hero-text" class="title">
+                        Go Explore
+                    </h1>
+                    <div id="logo" class="container">
+                        <img src="<%= ResolveUrl("~/Assets/Woodlands-Logo-Main.png") %>" />
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="store-or-blog-section" class="section">
+            <div class="container">
+                <div class="columns">
+                    <div id="store-column" class="column is-one-half">
+                        <h1 class="title">Shop</h1>
+                    </div>
+                    <div id="blog-column" class="column is-one-half">
+                        <h1 class="title">Blog</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    
 </asp:Content>
---
+++ WoodlandsSupplyCoWebApp/Scripts/main-animations.js
@@ -0,0 +1,55 @@
+﻿/* Hero Fade In Animations */
+
+$('#hero-text').each(function () {
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '#hero-text .letter',
+        opacity: [0, 1],
+        easing: "easeInOutQuad",
+        duration: 5000,
+        delay: function (el, i) {
+            return 150 * (i + 1)
+        }
+    }).add({
+        targets: '#hero-text',
+        opacity: 0,
+        duration: 5000,
+        easing: "easeOutExpo",
+        delay: 50
+    }).add({
+        targets: '#logo',
+        opacity: [0, 1],
+        easing: "easeInOutQuad",
+        duration: 5000,
+        delay: function (el, i) {
+            return 150 * (i + 1)
+        }
+    }).add({
+        targets: '#logo',
+        opacity: 0,
+        duration: 5000,
+        easing: "easeOutExpo",
+        delay: 50
+    });
+
+/* hamburger menu */
+document.addEventListener('DOMContentLoaded', function () {
+
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    if ($navbarBurgers.length > 0) {
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+                var target = $el.dataset.target;
+                var $target = document.getElementById(target);
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
+
+});
